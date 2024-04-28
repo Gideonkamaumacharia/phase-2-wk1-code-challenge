685bdb10d9fbfa9c48b81e0b23d21667afc1bfe0
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-28 09:22:19 +0300

    create AddTransactions

--- src/Components.js/AddTransactions.js
+++ src/Components.js/AddTransactions.js
@@ -1,21 +1,19 @@
-import React from "react"
+import React,{useState} from "react"
 
 function AddTransaction(){
-
-function Date(){
-
-}
-
-function Description(){
-
-}
-
-function Category(){
-    
-}
+    const [date, setDate] = useState("")
+    const [description, setDescription] = useState("")
+    const [category, setCategory] = useState("")
+    const [amount, setAmount] = useState("")
 
     return(
+        <form>
+            <input type="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)}/>
+            <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+            <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)}/>
+            <input type="number" placeholder="Amount" value={amount}/>
 
+        </form>
     )
 }
 
